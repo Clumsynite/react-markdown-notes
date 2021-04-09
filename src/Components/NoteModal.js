9a082992f3c587514733c685ba1b44bc51a23cbf
Rishabh Pathak <clusmsyknight@gmail.com>
2021-04-09 14:58:01 +0530

    updated modal to switch between view and edit note

--- src/Components/NoteModal.js
+++ src/Components/NoteModal.js
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
-import { Modal } from "antd";
+import { Modal, Row, Col, Switch } from "antd";
+import moment from "moment";
+import ReactCardFlip from "react-card-flip";
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
 
 export default function NoteModal({ visible, setVisible }) {
+  const [note, setNote] = useState("");
+  const [view, setView] = useState(false);
+
+  const NoteInput = () => (
+    <textarea
+      className="form-control"
+      style={{ height: "40vh", resize: "none" }}
+      value={note}
+      onChange={(e) => setNote(e.target.value)}
+    />
+  );
+
+  const NoteView = () => {
+    return <div className="form-control" style={{ height: "40vh" }}>
+      <ReactMarkdown plugins={[gfm]} children={note} />
+    </div>;
+  };
+
   return (
     <Modal
-      title="Add New Note"
+      title={`Add New Note - ${moment().fromNow()}`}
       centered
       visible={visible}
       onOk={() => console.log("OK Clicked inside Modal")}
@@ -12,10 +34,25 @@ export default function NoteModal({ visible, setVisible }) {
         console.log("Modal Cancelled");
         setVisible(false);
       }}
+      footer={null}
+      width={"50%"}
     >
-      <p>some contents...</p>
-      <p>some contents...</p>
-      <p>some contents...</p>
+      <div>
+        <ReactCardFlip isFlipped={view} flipDirection="horizontal">
+          <NoteView />
+          <NoteInput />
+        </ReactCardFlip>
+        <Row style={{ paddingTop: 10 }} justify="end">
+          {!view && <Col span={2}>View</Col>}
+          <Col span={2}>
+            <Switch
+              checkedChildren={"View"}
+              checked={view}
+              onChange={setView}
+            />
+          </Col>
+        </Row>
+      </div>
     </Modal>
   );
 }
